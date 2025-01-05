cda48ea132e739e5e85c1aaa020ad4149bcf9a07
somandhir <somandhir@gmail.com>
2025-01-06 00:07:58 +0530

    add drag drop feature

--- dist/script.js
+++ dist/script.js
@@ -7,18 +7,45 @@ inputBox.addEventListener("keydown", function (event) {
   }
 });
 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  var draggedItem = document.getElementById(data);
+
+    if (ev.target && ev.target.tagName === "LI") {
+    ev.target.parentNode.insertBefore(draggedItem, ev.target.nextSibling);
+  }
+  
+}
+
 function addTask() {
   if (inputBox.value === "") {
     alert("Enter a task first");
   } else {
     let li = document.createElement("li");
+    let taskId = "task-" + Date.now(); // Creating a unique ID using timestamp
+    li.id = taskId;  // Assigning the unique ID to the list item
+
     li.innerHTML = inputBox.value;
     li.classList.add("text-slate-100");
+    li.setAttribute("draggable", "true");
+    li.setAttribute("ondragstart", "drag(event)"); 
+    li.setAttribute("ondrop", "drop(event)");
+    li.setAttribute("ondragover", "allowDrop(event)");
     listCont.appendChild(li);
 
     let iconContainer = document.createElement("div");
     iconContainer.style.display = "flex";
     iconContainer.style.gap = "8px";
+    iconContainer.style.marginLeft = "1.5rem";
 
     let deleteIcon = document.createElement("img");
     deleteIcon.src = "/To-Do list 2.0/images/delete.png";
@@ -34,8 +61,18 @@ function addTask() {
     editIcon.style.height = "16px";
     editIcon.style.cursor = "pointer";
 
+    let dragIcon = document.createElement("img");
+    dragIcon.src = "/To-Do list 2.0/images/drag-down_15442899.png";
+    dragIcon.alt = "Drag";
+    dragIcon.style.width = "16px";
+    dragIcon.style.height = "16px";
+    dragIcon.style.cursor = "pointer";
+    
+
     iconContainer.appendChild(deleteIcon);
     iconContainer.appendChild(editIcon);
+    iconContainer.appendChild(dragIcon);
+    
 
     li.appendChild(iconContainer);
   }
